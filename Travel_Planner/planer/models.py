29d91ad7c74f4c5aabfb0f2972b1c8daae978a06
Olesia <Olesiakostak@gmail.com>
2026-04-06 13:33:59 +0200

    Models "TravelProject" and "Place" created

--- Travel_Planner/planer/models.py
+++ Travel_Planner/planer/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class TravelProject(models.Model):
+    name = models.CharField(max_length=256)
+    start_date = models.DateField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Travel project '{self.name}'"
+
+
+class Place(models.Model):
+    external_id = models.CharField(max_length=256)
+    project = models.ForeignKey(TravelProject, on_delete=models.CASCADE, related_name='places')
+    visited = models.BooleanField(default=False)
+    notes = models.TextField(null=True, blank=True)
+
+    class Meta:
+        unique_together = ('external_id', 'project')
+
+    def __str__(self):
+        return f"Place id: {self.external_id}. Project: {self.project.name}'"
